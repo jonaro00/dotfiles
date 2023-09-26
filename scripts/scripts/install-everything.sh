3d811b7d14aa6cede7b68919856afddb8c48e8e5
jonaro00 <54029719+jonaro00@users.noreply.github.com>
2023-09-27 01:10:24 +0200

    update installer

--- scripts/scripts/install-everything.sh
+++ scripts/scripts/install-everything.sh
@@ -16,16 +16,17 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/too
 
 ####### General
 
-sudo apt install -y cmake libssl-dev pkg-config
+sudo apt install -y cmake libssl-dev make pkg-config
+sudo apt install -y cowsay curl gh htop jq nmap xxd
 
 ####### Rust
 
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- --no-modify-path
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y --no-modify-path
 
 curl -L --proto '=https' --tlsv1.2 -sSf https://raw.githubusercontent.com/cargo-bins/cargo-binstall/main/install-from-binstall-release.sh | bash
 
 cargo binstall -y --locked sccache
 echo -e "[build]\nrustc-wrapper = \"$HOME/.cargo/bin/sccache\"" > ~/.cargo/config.toml
 
-cargo binstall -y --locked bacon bandwhich bat bore-cli bottom cargo-edit cargo-expand cargo-modules cargo-shuttle cargo-watch difftastic diskus du-dust exa fd-find gping hexyl onefetch ripgrep rust-script starship trunk
+cargo binstall -y --locked bacon bandwhich bat bore-cli bottom cargo-edit cargo-expand cargo-modules cargo-shuttle cargo-watch difftastic diskus du-dust exa fd-find gping hexyl onefetch ripgrep rust-script starship trunk zellij
 cargo install --locked cargo-info
