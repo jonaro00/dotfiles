0ae1d477abb72674dc9507917228cf63fe06bddf
jonaro00 <54029719+jonaro00@users.noreply.github.com>
2023-09-27 05:05:48 +0200

    fix installation

--- scripts/scripts/install-everything.sh
+++ scripts/scripts/install-everything.sh
@@ -4,11 +4,9 @@ if [ "$1" != "--zsh" ]; then
     sudo apt update
     sudo apt install -y zsh
     chsh -s $(which zsh)
-    zsh .install-everything.sh --zsh
+    zsh scripts/scripts/install-everything.sh --zsh
     exit 0
 fi
-
-[ "$SHELL" != "/usr/bin/zsh" ] && echo "Not in zsh" && exit 1
 
 ####### Shell
 
@@ -17,16 +15,22 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/too
 ####### General
 
 sudo apt install -y cmake libssl-dev make pkg-config
+sudo ln -s /usr/lib/libssl.so /usr/lib/libssl.so.1.1
 sudo apt install -y cowsay curl gh htop jq nmap xxd
 
 ####### Rust
 
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y --no-modify-path
+source "$HOME/.cargo/env"
 
 curl -L --proto '=https' --tlsv1.2 -sSf https://raw.githubusercontent.com/cargo-bins/cargo-binstall/main/install-from-binstall-release.sh | bash
 
-cargo binstall -y --locked sccache
+cargo install --locked sccache
 echo -e "[build]\nrustc-wrapper = \"$HOME/.cargo/bin/sccache\"" > ~/.cargo/config.toml
 
-cargo binstall -y --locked bacon bandwhich bat bore-cli bottom cargo-edit cargo-expand cargo-modules cargo-shuttle cargo-watch difftastic diskus du-dust exa fd-find gping hexyl onefetch ripgrep rust-script starship trunk zellij
+cargo binstall -y --locked atuin bacon bandwhich bat bore-cli bottom cargo-edit cargo-expand cargo-modules cargo-shuttle cargo-watch difftastic diskus du-dust exa fd-find gping hexyl onefetch ripgrep rust-script starship trunk zellij zoxide
 cargo install --locked cargo-info
+
+####### Cleanup
+
+mv ~/.zshrc /tmp
